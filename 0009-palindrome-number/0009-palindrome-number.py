8b9b8b5b160916bbdcd2db85ca67a44c5eec1e39
shweta2892 <shwetamishra2892@gmail.com>
2025-06-21 21:29:03 +0530

    Time: 8 ms (52.34%), Space: 17.6 MB (88.76%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -3,12 +3,12 @@ class Solution:
         if x < 0:
             return False
         
-        rev = 0
+        reverse = 0
         num = x
         
         while num != 0:
-            rev = rev * 10 + num % 10
+            reverse = reverse * 10 + num % 10
             num = num // 10
         
-        return rev == x
+        return reverse == x
         
