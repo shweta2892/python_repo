2f21170b00f494aa975e4b37296febaaf4770d4f
shweta2892 <shwetamishra2892@gmail.com>
2025-06-21 21:23:08 +0530

    Time: 408 ms (34.18%), Space: 18.1 MB (16.26%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -2,7 +2,7 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
         n = len(nums)
-        rs = []
+        result = []
         
         for i in range(n - 3):
             if i > 0 and nums[i] == nums[i - 1]:
@@ -19,7 +19,7 @@ class Solution:
                     elif sum < target:
                         left += 1
                     else:
-                        rs.append([nums[i], nums[j], nums[left], nums[right]])
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
                         while left < right and nums[left] == nums[left + 1]:
                             left += 1
                         while left < right and nums[right] == nums[right - 1]:
@@ -27,5 +27,5 @@ class Solution:
                         left += 1
                         right -= 1
 
-        return rs 
+        return result 
         
